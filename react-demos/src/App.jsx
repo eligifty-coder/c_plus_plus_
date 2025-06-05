22c3e82d3579a57ce001415a7938ad1088644aa0
eligifty-coder <gift.weike@gmail.com>
2025-06-05 17:08:14 +0100

    learnign JSX rules

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -1,8 +1,8 @@
 import Add from './component/Add';
 import Footer from './component/Footer';
 import Header from './component/Header';
-import Main from './component/Main';
 import WelcomeMessage from './component/WelcomeMessage';
+import JsxRules from './JsxRules';
 const App = () => {
   return (
     <section id = "section">
@@ -11,6 +11,7 @@ const App = () => {
       <article>
         <h2>Welcome to React</h2>
         <p className="text">Paragraph Content</p>
+        <JsxRules/>
       </article>
     </section>
   )
---
+++ react-demos/src/JsxRules.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const JsxRules = () => {
+    return (
+    <div>
+        <h1>JSX Rules</h1>
+        <p>JSX must retuen a single parent element</p>
+        <p>JSX element must be properly closed</p>
+        <p>JSX attributes are written using camelCase (e.g ClassName instead of class)</p>
+    </div>
+    )
+}
+
+export default JsxRules
