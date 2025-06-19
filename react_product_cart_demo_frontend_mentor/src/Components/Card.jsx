3985e32e696a7f955a569d20a9760c8d25d2bae8
eligifty-coder <gift.weike@gmail.com>
2025-06-19 22:54:59 +0100

    added the increment and decrement functionality

--- react_product_cart_demo_frontend_mentor/src/Components/Card.jsx
+++ react_product_cart_demo_frontend_mentor/src/Components/Card.jsx
@@ -15,26 +15,36 @@ const Card = ({item,setCount,count,items,setItems}) => {
             return ele
         }))
     }
-    
+    const handleDecrement = (e, item)=>{
+        e.preventDefault()
+        setItems(prev=>prev.map(ele=>{
+            if(item.id === ele.id){
+                const newCount = ele.count >0 ? ele.count - 1 :0
+                return {
+                    ...ele,
+                    count:newCount
+                }
+            }
+            return ele
+        }))
+    }
+    const displayBtn = !item.count ? (<button className={classes.cartBtn} onClick={(e) => handleCartButton(e, item)}>
+    <img src="../../icon-add-to-cart.svg" alt="" />Add to Cart
+</button>) : (<div className={classes.btnStyle }>
+            <button  onClick={(e) => handleDecrement(e, item)}>
+                <img src="../../icon-decrement-quantity.svg" alt="Remove Btn" />
+            </button>
+            <span>{item.count}</span>
+            <button  onClick={(e) => handleCartButton(e, item)}>
+                <img src="../../icon-increment-quantity.svg" alt="Add Btn" />
+            </button>
+            </div>)
     return (
         
         <li className={classes.card}>
             <div className={classes.positionedEle}>
             <img src={item.display} alt="load" />
-            
-            <button className={classes.cartBtn} onClick={(e) => handleCartButton(e, item)}>
-                <img src="../../icon-add-to-cart.svg" alt="" />Add to Cart
-            </button>
-            <div className={classes.btnStyle }>
-            <button  onClick={(e) => handleCartButton(e, item)}>
-                <img src="../../icon-decrement-quantity.svg" alt="Remove Btn" />
-            </button>
-            <span>0</span>
-            <button  onClick={(e) => handleCartButton(e, item)}>
-                <img src="../../icon-increment-quantity.svg" alt="Add Btn" />
-            </button>
-            </div>
-            
+            {displayBtn}
             </div>
             <section className={classes.text}>
                 <span className={classes.category}>{item.category}</span>
